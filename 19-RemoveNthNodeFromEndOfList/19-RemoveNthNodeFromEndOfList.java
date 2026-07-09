15e6be2a48efe6d7f7890d817563ab38b7b0ea28
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-09 15:10:51 +0530

    Create 19-RemoveNthNodeFromEndOfList

---
+++ 19-RemoveNthNodeFromEndOfList/19-RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:10:48 PM
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) fast = fast.next;
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
