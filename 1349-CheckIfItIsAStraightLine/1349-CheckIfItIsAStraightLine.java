20a7cb9cf9c1b72633a5552ce1ced2e0b67562f7
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-09 15:06:17 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 3:06:14 PM
+
+class Solution {
+    public boolean onLine(int[] p1, int[] p2, int[] p3){
+        int x = p1[0], y = p1[1], x1 = p2[0], y1 = p2[1], x2 = p3[0], y2 = p3[1];
+        return ((y - y1) * (x2 - x1) == (y2 - y1) * (x - x1));
+    }
+    public boolean checkStraightLine(int[][] coordinates) {
+        // base case:- there are only two points, return true
+        // otherwise, check each point lies on line using above equation.
+		
+        for(int i=2;i<coordinates.length;i++){
+            if(!onLine(coordinates[i], coordinates[0], coordinates[1]))
+                return false;
+        }
+        return true;
+    }
+}
