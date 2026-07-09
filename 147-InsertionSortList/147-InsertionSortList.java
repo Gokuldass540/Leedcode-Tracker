d99785de6f02d6e7ca0afc5e7a0992596d2c4a8c
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-09 15:08:46 +0530

    Create 147-InsertionSortList

---
+++ 147-InsertionSortList/147-InsertionSortList.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 3:08:43 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode ds=new ListNode(0);
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            ListNode prev = ds;
+            while(prev.next!=null&&prev.next.val<curr.val){
+                prev=prev.next;
+
+            }
+            curr.next=prev.next;
+            prev.next=curr;
+            curr=next;
+        }
+        return ds.next;
+    }
+}
