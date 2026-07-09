9d714623f103874c7e03cb41173c7d6009c77c7a
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-09 15:06:42 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 3:06:39 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        int mid;
+        while(i<=j){
+            mid=(i+j)/2;
+            if(nums[mid]==target){
+            return mid;
+            }else if(nums[mid]>target){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+        return -1;
+    }
+}
