72036db64d23739654a796905d793241ac70d8a1
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-09 15:11:11 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 3:11:09 PM
+class Solution {
+    public int reverse(int x) {
+        boolean isNegative = false;
+        if (x < 0) {
+            isNegative = true;
+            x = -x;
+        }
+
+        int res = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (res > (Integer.MAX_VALUE - digit) / 10) {
+                return 0;
+            }
+            res = (res * 10) + digit;
+        }
+
+        return isNegative ? -res : res;        
+    }
+}
