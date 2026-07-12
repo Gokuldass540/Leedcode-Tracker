063dcbc30bd44c42c8faf5543faf644e99893b2f
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-07-12 21:12:54 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/version/java/441-ArrangingCoins_20260712_211253.java
@@ -0,0 +1,16 @@
+// Last updated: 7/12/2026, 9:12:53 PM
+1class Solution {
+2    public int findContentChildren(int[] g, int[] s) {
+3        Arrays.sort(g);
+4        Arrays.sort(s);
+5        int contentChildren=0;
+6        int cookieIndex=0;
+7        while(cookieIndex<s.length&&contentChildren<g.length){
+8            if(s[cookieIndex]>=g[contentChildren]){
+9                contentChildren++;
+10            }
+11            cookieIndex++;
+12        }
+13        return contentChildren;
+14    }
+15}
