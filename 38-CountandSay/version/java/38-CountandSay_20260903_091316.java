def365a1c85f576fd1367f99d06c3862d174ca9e
Gokuldass540 <gokuldassvgokuldass@gmail.com>
2026-09-03 09:13:20 +0530

    Create 38-CountandSay

---
+++ 38-CountandSay/version/java/38-CountandSay_20260903_091316.java
@@ -0,0 +1,11 @@
+// Last updated: 9/3/2026, 9:13:16 AM
+1class Solution {
+2    public List<Integer> grayCode(int n) {
+3        int total=1<<n;
+4        List<Integer> gray=new ArrayList<>();
+5        for(int i=0;i<total;i++){
+6            gray.add(i^(i>>1));
+7        }
+8        return gray;
+9    }
+10}
